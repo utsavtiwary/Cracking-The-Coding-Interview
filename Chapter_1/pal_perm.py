c3cc87995f51a6730e56d3bc0ad16adc82598e8a
Utsav Tiwary <utsavtiwary@gmail.com>
2020-02-15 13:26:38 +0000

    Upload Palindrome Permutation from Chapter 1

---
+++ Chapter_1/pal_perm.py
@@ -0,0 +1,33 @@
+# Check if a given string is a permutation of a palindrome
+# EXAMPLE:
+# Input: Tact Coa
+# Output: True (permutations: "taco cat", "atco cta", etc.)
+
+import re
+
+def clean_string(string):
+	regex = re.compile('[^a-zA-z]')
+	return regex.sub('', string).lower()
+
+def pal_perm (string):
+
+	# Get rid of any whitespace or non-letter characters
+	string = clean_string(string)
+
+	char_count = set()
+	for c in string:
+		if c in char_count:
+			char_count.remove(c)
+		else:
+			char_count.add(c)
+
+	return len(char_count) <= 1
+
+print("Taco Cat:", pal_perm("Taco Cat"))
+# print True
+
+print("Taco59Cat:", pal_perm("Taco59Cat"))
+# print True
+
+print("FooBar:", pal_perm("FooBar"))
+# print False
